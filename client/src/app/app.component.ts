c229d9f225ee22616e4ab83bb732b842af50ba07
miroslav-petro <miroslav.petro@vissim.no>
2024-11-07 13:37:30 +0100

    add Tailwind CSS integration and PocketBase for post management

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import PocketBase from 'pocketbase'
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
+  public pb = new PocketBase('https://demoapi.miroslavpetro.com');
+  public posts = signal<Array<{title: string, content: string, created: string}>>([]);
+  constructor() {
+    this._loadPosts();
+    this.pb.collection('posts').subscribe('*', (e)=>{
+      this._loadPosts()
+    });
+   
+  }
+
+  private _loadPosts(){
+    this.pb.collection('posts').getFullList().then((posts) => {
+      this.posts.set(
+        posts.map((post) => {
+          return {
+            title: post['title'],
+            content: post['content'],
+            created: new Date(post.created).toLocaleString()
+          }
+        })
+      )
+    })
+  }
 }
